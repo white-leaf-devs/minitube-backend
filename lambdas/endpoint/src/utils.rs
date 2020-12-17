d48af5bb85180e55472bcb0105ecc4513c6bd5d7
Kevin Del Castillo <quebin31@gmail.com>
2020-12-16 20:46:11 -0500

    Added id utilities

---
+++ lambdas/endpoint/src/utils.rs
@@ -0,0 +1,17 @@
+pub const ALPHABET: [char; 63] = [
+    '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+    'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+
+pub const ID_LEN: usize = 22;
+
+pub fn generate_id() -> String {
+    nanoid::nanoid!(ID_LEN, &ALPHABET)
+}
+
+pub fn is_valid_id(id: &str) -> bool {
+    id.len() == ID_LEN && id.chars().all(|c| ALPHABET.contains(&c))
+}
+
