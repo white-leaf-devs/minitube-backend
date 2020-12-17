ba500b5d7061ea99b7ed0948d2641ef604593603
Kevin Del Castillo <quebin31@gmail.com>
2020-12-17 16:11:52 -0500

    Remove colored output.

--- lambdas/endpoint/src/main.rs
+++ lambdas/endpoint/src/main.rs
@@ -3,7 +3,6 @@ pub mod routes;
 pub mod utils;
 
 use anyhow::Result as AnyResult;
-use fern::colors::{Color, ColoredLevelConfig};
 use fern::Dispatch;
 use lambda_http::{lambda, Body, IntoResponse, Request, Response};
 use lambda_runtime::error::HandlerError;
@@ -33,24 +32,23 @@ fn handler(req: Request, _: Context) -> Result<Response<Body>, HandlerError> {
 }
 
 fn main() -> AnyResult<()> {
-    let colors = ColoredLevelConfig::default()
-        .info(Color::Cyan)
-        .trace(Color::BrightBlue)
-        .debug(Color::BrightMagenta);
-
     Dispatch::new()
         .level(LevelFilter::Info)
         .level_for("endpoint", LevelFilter::Debug)
         .level_for("lambda_http", LevelFilter::Debug)
-        .chain(Dispatch::new().format(move |out, msg, rec| {
-            out.finish(format_args! {
-                "{}[{}][{}] {}",
-                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                colors.color(rec.level()),
-                rec.target(),
-                msg
-            })
-        }))
+        .chain(
+            Dispatch::new()
+                .format(move |out, msg, rec| {
+                    out.finish(format_args! {
+                        "{}[{}][{}] {}",
+                        chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                        rec.level(),
+                        rec.target(),
+                        msg
+                    })
+                })
+                .chain(std::io::stdout()),
+        )
         .apply()?;
 
     log::info!("Registering handler");
