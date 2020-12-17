f943e306d1d343b52d78527dab74cebdb705a955
limonadev <limonadev@whiteleaf.dev>
2020-12-17 15:48:19 -0500

    Functional version of generate preview in lambda

--- lambdas/generate-preview/main.py
+++ lambdas/generate-preview/main.py
@@ -1,5 +1,13 @@
+import boto3
+import json
 import cv2
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
 from PIL import Image
+
+s3_client = boto3.client('s3')
 
 def getFrames(video, start_frame, number=30):
     frames = []
@@ -16,7 +24,7 @@ def getFrames(video, start_frame, number=30):
         counter += 1
 
     return frames
-
+    
 def buildPreview(in_filename, out_filename):
     video = cv2.VideoCapture(in_filename)
 
@@ -25,5 +33,21 @@ def buildPreview(in_filename, out_filename):
 
     frames[0].save(out_filename, save_all=True, append_images=frames[1:], optimize=True)
 
-if __name__ == '__main__':
-    buildPreview('La dura vida de Rubius.mp4', 'test.gif')
+def lambda_handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+        tmpkey = key.replace('/', '')
+        
+        tmpkey_no_extension = os.path.splitext(tmpkey)[0]
+        
+        file_id = uuid.uuid4()
+        
+        download_path = '/tmp/{}{}'.format(file_id, tmpkey)
+        print(download_path)
+        upload_path = '/tmp/resized-{}.gif'.format(tmpkey_no_extension)
+        print(upload_path)
+        s3_client.download_file(bucket, key, download_path)
+        
+        buildPreview(download_path, upload_path)
+        s3_client.upload_file(upload_path, '{}-resized'.format(bucket), f'{tmpkey_no_extension}.gif')
