591d64fab980257606c63beb4802d49250cdaacf
Kevin Del Castillo <quebin31@gmail.com>
2020-12-18 18:18:35 -0500

    Formatted code

--- lambdas/generate-preview/main.py
+++ lambdas/generate-preview/main.py
@@ -9,45 +9,51 @@ from PIL import Image
 
 s3_client = boto3.client('s3')
 
-def getFrames(video, start_frame, number=30):
+
+def get_frames(video, start_frame, number=30):
     frames = []
 
     success = True
     counter = 0
-    while success and counter<start_frame+number:
+    while success and counter < start_frame+number:
         success, pixels = video.read()
 
         if counter >= start_frame:
-            img = Image.fromarray(pixels.astype('uint8'), 'RGB').resize((240,135))
+            img = Image.fromarray(pixels.astype('uint8'),
+                                  'RGB').resize((240, 135))
             frames.append(img)
-        
+
         counter += 1
 
     return frames
-    
-def buildPreview(in_filename, out_filename):
+
+
+def build_preview(in_filename, out_filename):
     video = cv2.VideoCapture(in_filename)
 
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    frames = getFrames(video, frame_count/2)
+    frames = get_frames(video, frame_count/2)
 
-    frames[0].save(out_filename, save_all=True, append_images=frames[1:], optimize=True)
+    frames[0].save(out_filename, save_all=True,
+                   append_images=frames[1:], optimize=True)
+
 
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = unquote_plus(record['s3']['object']['key'])
         tmpkey = key.replace('/', '')
-        
+
         tmpkey_no_extension = os.path.splitext(tmpkey)[0]
-        
+
         file_id = uuid.uuid4()
-        
+
         download_path = '/tmp/{}{}'.format(file_id, tmpkey)
         print(download_path)
         upload_path = '/tmp/preview-{}.gif'.format(tmpkey_no_extension)
         print(upload_path)
         s3_client.download_file(bucket, key, download_path)
-        
-        buildPreview(download_path, upload_path)
-        s3_client.upload_file(upload_path, 'minitube.previews', f'{tmpkey_no_extension}.gif', ExtraArgs={'ACL': 'public-read'})
+
+        build_preview(download_path, upload_path)
+        s3_client.upload_file(upload_path, 'minitube.previews',
+                              f'{tmpkey_no_extension}.gif', ExtraArgs={'ACL': 'public-read'})
--- lambdas/generate-thumbnails/main.py
+++ lambdas/generate-thumbnails/main.py
@@ -10,8 +10,7 @@ import base64
 s3_client = boto3.client('s3')
 
 
-
-def getThumbnails(video, total_frames, jumps=5):
+def get_thumbnails(video, total_frames, jumps=5):
     thumbnails = []
 
     selected_frames = [i*(total_frames//jumps) for i in range(jumps)]
@@ -22,23 +21,23 @@ def getThumbnails(video, total_frames, jumps=5):
         success, pixels = video.read()
 
         if counter in selected_frames:
-            img = cv2.resize(pixels, (240,135))
+            img = cv2.resize(pixels, (240, 135))
             thumbnails.append(img)
-        
+
         counter += 1
 
     return thumbnails
 
 
-def buildThumbnails(in_filename, out_file_prefix):
+def build_thumbnails(in_filename, out_file_prefix):
     video = cv2.VideoCapture(in_filename)
 
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    thumbnails = getThumbnails(video, frame_count)
+    thumbnails = get_thumbnails(video, frame_count)
 
     thumbnails_as_base64 = []
 
-    for i,thum in enumerate(thumbnails):
+    for i, thum in enumerate(thumbnails):
         filename = f'{out_file_prefix}_{i}.png'
         cv2.imwrite(filename, thum)
 
@@ -48,27 +47,27 @@ def buildThumbnails(in_filename, out_file_prefix):
 
     return thumbnails_as_base64
 
+
 def lambda_handler(event, context):
     bucket = 'minitube.videos'
     key = event['video_id']
 
     tmpkey = key.replace('/', '')
-        
+
     tmpkey_no_extension = os.path.splitext(tmpkey)[0]
-    
+
     file_id = uuid.uuid4()
-    
+
     download_path = '/tmp/{}{}'.format(file_id, tmpkey)
     print(download_path)
     s3_client.download_file(bucket, key, download_path)
-    
+
     upload_path_prefix = '/tmp/thumb-{}'.format(tmpkey_no_extension)
     print(upload_path_prefix)
-    
-    thumbnails = buildThumbnails(download_path, upload_path_prefix)
-    
+
+    thumbnails = build_thumbnails(download_path, upload_path_prefix)
+
     return {
         'statusCode': 200,
         'body': json.dumps(thumbnails)
     }
-        
