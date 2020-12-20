53e9d4a3d6e7970b1071e6b23ff5110262508e2a
Kevin Del Castillo <quebin31@gmail.com>
2020-12-20 11:23:59 -0500

    Transform labels to lowercase, version 0.2.4.

--- lambdas/label-thumbnail/src/data.rs
+++ lambdas/label-thumbnail/src/data.rs
@@ -17,16 +17,18 @@ impl From<Vec<Label>> for Labels {
         let mut labels: Vec<_> = prelabels
             .iter()
             .cloned()
-            .filter_map(|label| label.name)
+            .filter_map(|label| label.name.map(|s| s.to_lowercase()))
             .collect();
 
         let parent_labels = prelabels
             .iter()
             .cloned()
             .filter_map(|label| {
-                label
-                    .parents
-                    .map(|parents| parents.into_iter().filter_map(|parent| parent.name))
+                label.parents.map(|parents| {
+                    parents
+                        .into_iter()
+                        .filter_map(|parent| parent.name.map(|s| s.to_lowercase()))
+                })
             })
             .flatten();
 
