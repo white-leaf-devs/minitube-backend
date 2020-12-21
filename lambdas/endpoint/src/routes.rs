8024e26b0a808fc1b0aacfaa750e4147a4005140
Kevin Del Castillo <quebin31@gmail.com>
2020-12-21 11:19:53 -0500

    Move search logic to external lambda.

--- lambdas/endpoint/src/routes.rs
+++ lambdas/endpoint/src/routes.rs
@@ -1,24 +1,18 @@
-use std::collections::HashMap;
 use std::time::Duration;
 
-use common_macros::hash_map;
 use json::Value;
 use netlify_lambda_http::http::Method;
 use netlify_lambda_http::{Body, IntoResponse, Request, RequestExt, Response};
 use rusoto_core::Region;
 use rusoto_credential::{ChainProvider, ProvideAwsCredentials};
-use rusoto_dynamodb::{
-    AttributeValue, BatchGetItemInput, DynamoDb, DynamoDbClient, KeysAndAttributes,
-};
-use rusoto_lambda::{InvocationRequest, Lambda, LambdaClient};
 use rusoto_s3::util::{PreSignedRequest, PreSignedRequestOption};
 use rusoto_s3::PutObjectRequest;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use serde_json::{self as json, json};
 
 use crate::error::Error;
 use crate::utils::{generate_id, is_valid_id};
-use crate::validate_request;
+use crate::{invoke_lambda, validate_request};
 
 pub async fn create_video(req: Request) -> Result<Response<Body>, Error> {
     validate_request!(Method::GET, req);
@@ -71,30 +65,12 @@ pub async fn generate_thumbnail(req: Request) -> Result<Response<Body>, Error> {
         return Err(Error::bad_request("Invalid `video_id`"));
     }
 
-    let lambda = LambdaClient::new(Region::UsEast1);
     let payload = json!({
         "video_key": format!("{}.mp4", body.video_id),
         "timestamp": body.timestamp,
     });
 
-    println!("Payload for lambda: {:#?}", payload);
-    let input = InvocationRequest {
-        function_name: "GenerateThumbnailLambda".to_string(),
-        payload: Some(payload.to_string().into()),
-        ..Default::default()
-    };
-
-    println!("Invocation request: {:#?}", input);
-    let output = lambda.invoke(input).await?;
-    println!("Lambda output: {:#?}", output);
-
-    let payload = output
-        .payload
-        .map(|bytes| String::from_utf8(bytes.to_vec()))
-        .transpose()?
-        .unwrap_or_default();
-
-    println!("Lambda payload: {:?}", payload);
+    let (output, payload) = invoke_lambda!("GenerateThumbnailLambda", payload);
     if let Some(err) = output.function_error {
         Err(Error::internal_error(format!("{} ({})", err, payload)))
     } else {
@@ -121,59 +97,16 @@ pub async fn detect_and_save_labels(req: Request) -> Result<Response<Body>, Erro
         return Err(Error::bad_request("Invalid `video_id`"));
     }
 
-    let lambda = LambdaClient::new(Region::UsEast1);
     let payload = json!({
         "video_id": body.video_id.clone(),
         "bucket": "minitube.thumbnails"
     });
 
-    println!("Payload for lambda: {:#?}", payload);
-    let input = InvocationRequest {
-        function_name: "LabelThumbnailLambda".to_string(),
-        payload: Some(payload.to_string().into()),
-        ..Default::default()
-    };
-
-    println!("Invocation request: {:#?}", input);
-    let output = lambda.invoke(input).await?;
-    println!("Lambda output: {:#?}", output);
-
-    let payload = output
-        .payload
-        .map(|bytes| String::from_utf8(bytes.to_vec()))
-        .transpose()?
-        .unwrap_or_default();
-
-    println!("Lambda payload: {:?}", payload);
+    let (output, payload) = invoke_lambda!("LabelThumbnailLambda", payload);
     if let Some(err) = output.function_error {
         Err(Error::internal_error(format!("{} ({})", err, payload)))
     } else {
         Ok(json::from_str::<Value>(&payload)?.into_response())
-    }
-}
-
-#[derive(Debug, Clone, Serialize)]
-struct VideoInfo {
-    labels: Vec<String>,
-    video_url: String,
-    preview_url: String,
-    thumbnail_url: String,
-}
-
-impl VideoInfo {
-    const S3_URL: &'static str = "https://s3.amazonaws.com";
-
-    fn new(labels: Vec<String>, video_id: &str) -> Self {
-        let video_url = format!("{}/minitube.videos/{}.mp4", Self::S3_URL, video_id);
-        let preview_url = format!("{}/minitube.previews/{}.gif", Self::S3_URL, video_id);
-        let thumbnail_url = format!("{}/minitube.thumbnails/{}.png", Self::S3_URL, video_id);
-
-        Self {
-            labels,
-            video_url,
-            preview_url,
-            thumbnail_url,
-        }
     }
 }
 
@@ -185,69 +118,15 @@ pub async fn search(req: Request) -> Result<Response<Body>, Error> {
 
     let query = query
         .get("query")
-        .ok_or_else(|| Error::bad_request("Missing `q` param"))?;
+        .ok_or_else(|| Error::bad_request("Missing `query` param"))?;
 
-    let labels: Vec<_> = query
-        .split(' ')
-        .map(|label| {
-            hash_map! {
-                "Label".to_string() => AttributeValue {
-                    s: Some(label.to_lowercase()),
-                    ..Default::default()
-                }
-            }
-        })
-        .collect();
+    let labels: Vec<_> = query.split(' ').map(ToOwned::to_owned).collect();
+    let payload = json!({ "labels": labels });
 
-    println!("Labels to search: {:?}", labels);
-    let db = DynamoDbClient::new(Region::UsEast1);
-    let input = BatchGetItemInput {
-        request_items: hash_map! {
-            "Labels".to_string() => KeysAndAttributes {
-                keys: labels,
-                ..Default::default()
-            }
-        },
-        ..Default::default()
-    };
-
-    println!("BatchGetItem request: {:#?}", input);
-    let output = db.batch_get_item(input).await?;
-
-    let labels_items = output
-        .responses
-        .map(|res| res.get("Labels").cloned())
-        .flatten();
-
-    println!("Items matching criteria: {:#?}", labels_items);
-    let json = if let Some(labels_items) = labels_items {
-        let simplified = labels_items.into_iter().filter_map(|item| {
-            let label = item.get("Label").cloned()?.s?;
-            let videos = item.get("Videos").cloned()?.ss?;
-
-            Some((label, videos))
-        });
-
-        let mut video_to_labels = HashMap::new();
-        for (label, videos) in simplified {
-            for video in videos {
-                video_to_labels
-                    .entry(video)
-                    .or_insert_with(Vec::new)
-                    .push(label.clone());
-            }
-        }
-
-        println!("Video to labels: {:#?}", video_to_labels);
-        let videos: Vec<_> = video_to_labels
-            .into_iter()
-            .map(|(video_id, labels)| VideoInfo::new(labels, &video_id))
-            .collect();
-
-        json!({ "videos": videos })
+    let (output, payload) = invoke_lambda!("SearchLabelsLambda", payload);
+    if let Some(err) = output.function_error {
+        Err(Error::internal_error(format!("{} ({})", err, payload)))
     } else {
-        json!({ "videos": Vec::<VideoInfo>::new() })
-    };
-
-    Ok(json.into_response())
+        Ok(json::from_str::<Value>(&payload)?.into_response())
+    }
 }
