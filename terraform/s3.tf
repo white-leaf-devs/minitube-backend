9f6c464607d059bef27a565babfb516c209579cb
Kevin Del Castillo <quebin31@gmail.com>
2020-12-20 19:49:27 -0500

    Remove code buckets and objects.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,29 +12,3 @@ resource "aws_s3_bucket" "previews" {
   bucket = "minitube.previews"
   acl    = "private"
 }
-
-resource "aws_s3_bucket" "code" {
-  bucket = "minitube.code"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_object" "opencv_layer" {
-  bucket = aws_s3_bucket.code.id
-  key    = "opencv-layer.zip"
-  source = "../layers/opencv-layer.zip"
-  etag   = filemd5("../layers/opencv-layer.zip")
-}
-
-resource "aws_s3_bucket_object" "generate_preview" {
-  bucket = aws_s3_bucket.code.id
-  key    = "generate-preview.zip"
-  source = "../generate-preview.zip"
-  etag   = filemd5("../generate-preview.zip")
-}
-
-resource "aws_s3_bucket_object" "generate_thumbnails" {
-  bucket = aws_s3_bucket.code.id
-  key    = "generate-thumbnails.zip"
-  source = "../generate-thumbnails.zip"
-  etag   = filemd5("../generate-thumbnails.zip")
-}
