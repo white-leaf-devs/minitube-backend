77f8e44f857ccadf775e4a0cdbd4ab58e495e205
Kevin Del Castillo <quebin31@gmail.com>
2020-12-28 10:03:43 -0500

    Fixed CORS on S3 buckets.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,14 +1,38 @@
 resource "aws_s3_bucket" "videos" {
   bucket = "minitube.videos"
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "GET", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket" "thumbnails" {
   bucket = "minitube.thumbnails"
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "GET", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket" "previews" {
   bucket = "minitube.previews"
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "GET", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 }
